2ec5608ff7c016f3d3e21b5886bfac5e36be32e0
shahae7 <shahaepno@gmail.com>
2026-05-15 16:45:37 +0530

    Fixed AuthContext imports

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import api from '../api/axios'
-import { useAuth } from '../context/AuthContext'
+import { useAuth } from "../AuthContext";
 import {Link,useNavigate} from 'react-router-dom'
 
 function Login() {
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import { useAuth } from '../context/AuthContext'
+import { useAuth } from "../AuthContext";
 import {
   useNavigate
 } from 'react-router-dom'
