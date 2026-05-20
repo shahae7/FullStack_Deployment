19968b1abf521a14b729183b33b6e8e47131f613
shahae7 <shahaepno@gmail.com>
2026-05-20 11:06:10 +0530

    update backend url to ngrok https

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: "http://3.109.184.218:8000/api/v1",
+ baseURL: "https://dingy-dodgy-unstylish.ngrok-free.dev/api/v1",
   withCredentials: false
 })
 
