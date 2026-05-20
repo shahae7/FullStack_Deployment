d0e99435107c8e6a1df20613a9ab58bcc65e1733
shahae7 <shahaepno@gmail.com>
2026-05-20 10:43:13 +0530

    update axios baseURL for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1"
+  baseURL: "http://3.109.184.218:8000/api/v1"
 })
 
 api.interceptors.request.use(
